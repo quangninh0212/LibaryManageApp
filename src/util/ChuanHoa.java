20e4f8fe8e896408769b518b8a75a8d1daf82302
Quang Ninh <dangninh138@gmail.com>
2024-11-06 19:39:58 +0700

    LibaryManageApp

---
+++ src/util/ChuanHoa.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author admin
+ */
+public class ChuanHoa {
+    public static String chuanHoaNgayThang(String ngaySinh) {
+        String arr[] = ngaySinh.split("/");
+        return arr[2] + "-" + arr[1] + "-" + arr[0];
+    }
+}
